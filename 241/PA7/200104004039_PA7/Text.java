3c5cd33778d0bc6f2d1a20cda3380633aba1408b
S. Zeynep Gürbüz <koioszey@gmail.com>
2025-05-05 09:21:35 +0300

    Delete 241 directory

--- 241/PA7/200104004039_PA7/Text.java
+++
@@ -1,5 +0,0 @@
-public class Text extends ObjectBase implements NonPlayable, NonVisual {
-    public Text() { super(); }
-
-    public Text(String name, String info) { super(name, info, "text"); }
-}
